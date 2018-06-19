4da5a61f5aa4b26c326fdc83935863b9b392e949
sefabey <sefabey@gmail.com>
2018-06-19 23:37:50 +0100

    add tutorials to try

---
+++ cartogram_001.R
@@ -0,0 +1,7 @@
+Things to try:
+    
+# not too bad    
+https://stackoverflow.com/questions/9319597/cartogram-choropleth-map-in-r
+
+# looking neat
+http://trucvietle.me/r/tutorial/2016/12/18/cartogram-plotting-using-r.html
