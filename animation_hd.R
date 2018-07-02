1d77eda603ab3fdef02250dd43fa6dc19e1ae03c
Sefa <sefabey@gmail.com>
2018-07-02 22:51:35 +0000

    a bit cleanup

--- animation_hd.R
+++ animation_hd.R
@@ -10,12 +10,7 @@ library(tidyverse)
 
 # choropleth=====
 world <- rgdal::readOGR('TM_WORLD_BORDERS-0.3/TM_WORLD_BORDERS-0.3.shp')
-map.world <- map_data(world) #getting world map in dataframe format to plot in ggplot2
 osce_data <- read_csv("osce_hate_incidents_2016_wide.csv") #reading in OSCE hate incidents
-map.world$iso3c <- countrycode::countrycode(map.world$region, origin = 'country.name', destination = 'iso3c' ) #getting country codes in iso3c format
-osce_map_joined <- left_join(map.world, osce_data, by = 'iso3c')
-osce_map_joined
-
 
 smaller.data_choro <- osce_data %>% dplyr::select(Country.Code = iso3c, hate_fig_civil = figures_civil_intern_orgs) %>% 
     na.omit() %>%
@@ -25,9 +20,9 @@ smaller.data_choro <- osce_data %>% dplyr::select(Country.Code = iso3c, hate_fig
 matched.indices <- match(world@data[, "ISO3"], smaller.data_choro[, "Country.Code"])
 world@data <- data.frame(world@data, smaller.data_choro[matched.indices, ])
 
-world_choro_civil <- fortify(world, region = "Country.Code")
-world_choro_civil <- merge(world_choro_civil, world@data, by.x = "id", by.y = "Country.Code")
-world_choro_civil$hate_fig_civil
+
+world_choro_civil <- tidy(world, region = "Country.Code") %>% left_join(world@data, by=c('id'= "Country.Code"))
+# world_choro_civil <- merge(world_choro_civil, world@data, by.x = "id", by.y = "Country.Code")
 
 choro_civil <- ggplot(world_choro_civil, aes(long, lat, group = group, fill = world_choro_civil$hate_fig_civil %>% log2))+ 
     # theme(legend.position="bottom")+ 
@@ -48,31 +43,27 @@ choro_civil
 
 # carto=======
 
-world <- rgdal::readOGR('TM_WORLD_BORDERS-0.3/TM_WORLD_BORDERS-0.3.shp')
-osce_data <- read_csv("osce_hate_incidents_2016_wide.csv") #reading in OSCE hate incidents
+# world <- rgdal::readOGR('TM_WORLD_BORDERS-0.3/TM_WORLD_BORDERS-0.3.shp')
+# osce_data <- read_csv("osce_hate_incidents_2016_wide.csv") #reading in OSCE hate incidents
+# 
+# smaller.data_carto <- osce_data %>% dplyr::select(Country.Code = iso3c, hate_fig_civil = figures_civil_intern_orgs) %>% 
+#     na.omit() %>%
+#     data.frame()
+# 
+# ## Join the two datasets using their common field
+# matched.indices <- match(world@data[, "ISO3"], smaller.data[, "Country.Code"])
+# world@data <- data.frame(world@data, smaller.data[matched.indices, ])
 
-smaller.data <- osce_data %>% dplyr::select(Country.Code = iso3c, hate_fig_civil = figures_civil_intern_orgs) %>% 
-    na.omit() %>%
-    data.frame()
-
-## Join the two datasets using their common field
-matched.indices <- match(world@data[, "ISO3"], smaller.data[, "Country.Code"])
-world@data <- data.frame(world@data, smaller.data[matched.indices, ])
-
-
-
-## Compute the cartogram transformation of each country using its population
-## with the degree of Gaussian blur = 0.5 (otherwise, it may not work)
-world.carto_civil <- cartogram (world, 'hate_fig_civil', itermax=3)
-plot(world.carto_civil)
+world_carto_civil <- cartogram (world, 'hate_fig_civil', itermax=7)
+plot(world_carto_civil)
 
 
 ## Convert the object into data frame
-world.carto_civil <- fortify(world.carto_civil, region = "Country.Code")
+world_carto_civil_tidy <- tidy(world_carto_civil, region = "Country.Code") %>% left_join(world@data, by=c('id'= "Country.Code"))
 ## Merge the cartogram transformation with the world map shapefile
-world.carto_civil <- merge(world.carto_civil, world@data, by.x = "id", by.y = "Country.Code")
+# world.carto_civil_tidy <- merge(world.carto_civil, world@data, by.x = "id", by.y = "Country.Code")
 
-carto_civil <- ggplot(world.carto_civil, aes(long, lat, group = group, fill = world.carto_civil$hate_fig_civil %>% log2))+
+carto_civil <- ggplot(world_carto_civil_tidy, aes(long, lat, group = group, fill = world_carto_civil_tidy$hate_fig_civil %>% log2))+
     geom_polygon()+
     # geom_text(data=world.f2, aes(x=longit, y=latid, label = label_text_civil),size=4)+ #cant ge this to work
     # geom_text(data=world.f2, aes(x=longit-3, y=latid, label = ISO3),size=4)+ #cant ge this to work
@@ -94,34 +85,34 @@ carto_civil
 
 # Give an id to every single point that compose the boundaries
 
-world.carto_civil$id=seq(1,nrow(world.carto_civil))
+world_carto_civil_tidy$id=seq(1,nrow(world_carto_civil_tidy))
 world_choro_civil$id=seq(1,nrow(world_choro_civil))
 
-world.carto_civil %>% colnames()
+world_carto_civil_tidy %>% colnames()
 world_choro_civil %>% colnames()
 
 # Bind both map info in a data frame. 3 states: map --> cartogram --> map
-data=rbind(world_choro_civil, world.carto_civil, world_choro_civil)
+data=rbind(world_choro_civil, world_carto_civil_tidy, world_choro_civil)
 world_choro_civil %>% nrow
-world.carto_civil %>% nrow
+world_carto_civil_tidy %>% nrow
 # Set transformation type + time
 data$ease="cubic-in-out"
 data$time=1
-152869+world.carto_civil %>% nrow
-data$time[152870:305737] <- 2
-data$time[305738:nrow(data)] <- 3
+152869+world_carto_civil_tidy %>% nrow
+data$time[152870:305729] <- 2
+data$time[305730:nrow(data)] <- 3
 
 # Calculate the transition between these 2 objects?
-dt <- tween_elements(data, time='time', group='id', ease='ease', nframes = 60)
+data_tween <- tween_elements(data, time='time', group='id', ease='ease', nframes = 60)
 
 # check a few frame
-ggplot() + geom_polygon(data = dt %>% filter(.frame==0) %>% arrange(order), aes(fill = hate_fig_civil, x = long, y = lat, group = group) , size=0, alpha=0.9)
-ggplot() + geom_polygon(data = dt %>% filter(.frame==5) %>% arrange(order), aes(fill = hate_fig_civil, x = long, y = lat, group = group) , size=0, alpha=0.9)
-ggplot() + geom_polygon(data = dt %>% filter(.frame==10) %>% arrange(order), aes(fill = hate_fig_civil, x = long, y = lat, group = group) , size=0, alpha=0.9)
+ggplot() + geom_polygon(data = data_tween %>% filter(.frame==0) %>% arrange(order), aes(fill = hate_fig_civil, x = long, y = lat, group = group) , size=0, alpha=0.9)
+ggplot() + geom_polygon(data = data_tween %>% filter(.frame==5) %>% arrange(order), aes(fill = hate_fig_civil, x = long, y = lat, group = group) , size=0, alpha=0.9)
+ggplot() + geom_polygon(data = data_tween %>% filter(.frame==10) %>% arrange(order), aes(fill = hate_fig_civil, x = long, y = lat, group = group) , size=0, alpha=0.9)
 
 # Plot
 p= ggplot() + 
-    geom_polygon(data = dt  %>% arrange(order) , 
+    geom_polygon(data = data_tween  %>% arrange(order) , 
                  aes(fill = hate_fig_civil %>% log2, x = long, y = lat, group = group, frame=.frame) 
                  , size=1, alpha=1) +
     scale_fill_gradientn (name="Incident Counts",
