fd05de80fe40f6a8e1ec614e6ddc5058b331bffb
Sefa <sefabey@gmail.com>
2018-07-04 11:48:50 +0000

    add file renderer

--- reprex_gganimate/gganimate_thomas_reprex.r
+++ reprex_gganimate/gganimate_thomas_reprex.r
@@ -3,6 +3,10 @@
 # devtools::install_github('thomasp85/transformr', force=T)
 # devtools::install_github("tidyverse/ggplot2", force=T)
 
+library(tweenr)
+library(transformr)
+library(tidyverse)
+library(ggplot2)
 library(gganimate) # thomasp85/gganimate 
 library(cartogram)
 # devtools::install_github('jbaileyh/geogrid')
@@ -17,7 +21,7 @@ us <- ne_states('united states of america', returnclass = 'sf')
 us <- us[!us$woe_name %in% c('Alaska', 'Hawaii'), ]
 us <- st_transform(us, '+proj=eqdc +lat_0=39 +lon_0=-96 +lat_1=33 +lat_2=45 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs')
 # Population data from https://www.census.gov/data/tables/2017/demo/popest/state-total.html
-pop <- read.csv('https://www2.census.gov/programs-surveys/popest/datasets/2010-2017/national/totals/nst-est2017-alldata.csv',
+pop <- read.csv('https://www2.census.gov/programs-surveys/popest/datasets/2010-2017/national/totals/nst-est2017-popchg2010_2017.csv',
                 header = TRUE, skip = 0, check.names = F) #my berst bet is this data
 us$pop <- pop[match(us$name, pop$NAME),'POPESTIMATE2017' ] # selected 2017 population as weights 
 
@@ -40,7 +44,7 @@ us_sq$type <- types[4]
 us_all <- rbind(us, us_hex[, names(us)], us_ca[, names(us)], us_sq[, names(us)])
 us_all$type <- factor(us_all$type, levels = types)
 
-ggplot(us_all) + 
+var <-  ggplot(us_all) + 
     geom_sf(aes(fill = pop, group = name)) + 
     scale_fill_scico(palette = 'lapaz') + 
     coord_sf(datum = NA) +
@@ -50,4 +54,13 @@ ggplot(us_all) +
     labs(title = 'Showing {closest_state}', 
          fill = 'Population') +
     transition_states(type, 2, 1)
+
+var
+
+animate(var,nframes = 100, fps = 10,
+        renderer = file_renderer(dir = "reprex_gganimate/file_renderer/", prefix = "gganim_plot", overwrite = FALSE))
+
+animate(var, nframes = 30, 'reprex_using_animate.gif',
+        renderer = file_renderer(dir = "~", prefix = "gganim_plot", overwrite = FALSE))
+
 
